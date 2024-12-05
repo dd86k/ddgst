861fe26b9a2ba6c6c4a386d3fd12a1ad57de9d28
dd86k <dd@dax.moe>
2024-12-05 14:17:26 -0500

    mtdir: To keep the threads occupied, try another thread if its mailbox is full

--- src/mtdir.d
+++ src/mtdir.d
@@ -9,6 +9,8 @@ import std.concurrency;
 import std.file;
 import std.path : globMatch, baseName;
 import std.parallelism : totalCPUs;
+import core.thread.osthread : Thread;
+import std.datetime : dur;
 
 //TODO: Find a way to construct dirEntries iterator.
 //      Can't access DirIterator.this, can't re-use FilterResults template,
@@ -34,7 +36,7 @@ import std.parallelism : totalCPUs;
 void dirEntriesMT(string path, string pattern, SpanMode mode, bool follow,
     immutable(void)* function() fnspawn,
     void function(DirEntry, immutable(void)*) fnentry,
-    int threads = 0, int mailboxSize = 5)
+    int threads = 0, int mailboxSize = 2)
 {
     if (path == null)
         throw new Exception("Path is not defined");
@@ -71,46 +73,54 @@ void dirEntriesSTImpl(string path, string pattern, SpanMode mode, bool follow,
     }
 }
 
-// When threads=0 or threads>1 is specified
+// threads >= 0
 void dirEntriesMTImpl(string path, string pattern, SpanMode mode, bool followLinks,
     immutable(void)* function() fnspawn,
     void function(DirEntry, immutable(void)*) fnentry,
     int threads, int mailboxSize)
 {
-    // Spawn n threads and assign them a queue size of 10.
-    // 10 because if we work on millions of file paths,
-    // the stack might suffer at higher count.
-    scope pool = new Tid[threads];
+    // Spawn n threads and assign them their queue size.
+    scope threadPool = new Tid[threads];
     for (int i; i < threads; ++i)
     {
         Tid tid = spawn(&dirEntriesMTWorker, thisTid, fnentry, fnspawn());
-        // NOTE: About OnCrowding
-        //       .ignore is a little suspicious, as at the end of the entries,
-        //       a thread's mailbox might be full, and ignore MsgDone messages.
-        setMaxMailboxSize(tid, mailboxSize, OnCrowding.block);
-        pool[i] = tid;
+        setMaxMailboxSize(tid, mailboxSize, OnCrowding.throwException);
+        threadPool[i] = tid;
     }
     
     // Send every thread a message to work on.
-    int cur; // Current thread index
+    int threadIndex; // Current thread index
     foreach (DirEntry entry; dirEntries(path, mode, followLinks))
     {
         if (pattern && globMatch(baseName(entry.name), pattern) == false)
             continue;
-        send(pool[cur], MsgEntry(entry));
-        if (++cur >= threads) cur = 0;
+    Lretry:
+        try send(threadPool[threadIndex], MsgEntry(entry));
+        catch (MailboxFull) // time to try another thread from pool
+        {
+            if (++threadIndex >= threads)
+            {
+                threadIndex = 0;
+                // Since this is the end of the list, wait for a little,
+                // maybe a thread will have some room again soon.
+                Thread.sleep(dur!"msecs"(500));
+            }
+            goto Lretry;
+        }
+        if (++threadIndex >= threads) threadIndex = 0;
     }
     
     // NOTE: Done/Ack messages are required to avoid an exception
     //       where the parent thread is terminated before the worker threads.
     // Push cancel request at the end of the queue.
-    foreach (ref tid; pool)
+    foreach (ref tid; threadPool)
     {
+        setMaxMailboxSize(tid, mailboxSize, OnCrowding.block);
         send(tid, MsgDone());
     }
     // Wait for confirmation, this is to avoid this thread
     // to finish before their children
-    foreach (ref tid; pool)
+    foreach (ref tid; threadPool)
     {
         receiveOnly!MsgDoneAck;
     }
@@ -127,17 +137,14 @@ void dirEntriesMTWorker(Tid parentTid,
     void function(DirEntry, immutable(void)*) fnuser,
     immutable(void) *uobj)
 {
-    bool cont = true;
-    while (cont)
-    {
-        receive(
-            (MsgEntry msg) {
-                fnuser(msg.entry, uobj);
-            },
-            (MsgDone msg) {
-                send(parentTid, MsgDoneAck());
-                cont = false;
-            }
-        );
-    }
+    bool working = true;
+    while (working) receive(
+        (MsgEntry msg) {
+            fnuser(msg.entry, uobj);
+        },
+        (MsgDone msg) {
+            send(parentTid, MsgDoneAck());
+            working = false;
+        }
+    );
 }
